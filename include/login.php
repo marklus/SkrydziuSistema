259c6c7cf40415032a0816c892d57e4c63feaa52
marklus <35917098+marklus@users.noreply.github.com>
2023-11-04 16:02:47 +0200

    Update login.php

--- include/login.php
+++ include/login.php
@@ -12,19 +12,56 @@ include("include/nustatymai.php");
 
 		<form action="proclogin.php" method="POST" class="login-box">            
         <center style="font-size:18pt; color: white;"><b>Prisijungimas</b></center>
-        <p style="text-align:left; color: white;">Vartotojo vardas:<br>
+        <p style="text-align:left; color: white;"><br>
+        <div class="user-box">
             <input class ="user-box" name="user" type="text" value="<?php echo $_SESSION['name_login'];  ?>"/><br>
             <?php echo $_SESSION['name_error']; 
 			?>
+            <label>Vartotojo vardas:</label>
+         </div>  
         </p>
-        <p style="text-align:left;  color: white;">Slaptažodis:<br>
+
+        <div class="user-box">
+        <p style="text-align:left;  color: white;"><br>
             <input class ="user-box" name="pass" type="password" value="<?php echo $_SESSION['pass_login']; ?>"/><br>
             <?php echo $_SESSION['pass_error']; 
 			?>
+            <label>Vartotojo slaptazodis:</label>
+        </div>  
         </p>  
+
+        <style>
+  .button-container {
+    text-align: left;
+    color: white;
+  }
+
+  .button-container a {
+    text-decoration: none;
+    color: white;
+    margin-right: 10px;
+  }
+
+  .button {
+    background-color: #007bff;
+    color: white;
+    padding: 5px 10px;
+    border: none;
+    cursor: pointer;
+    margin-right: 10px;
+  }
+
+  .button a {
+    text-decoration: none;
+    color: white;
+  }
+</style>
+
+
+
         <p style="text-align:left;  color: white;">
-            <input type="submit" name="login" value="Prisijungti"/>   
-            <input type="submit" name="problem" value="Pamiršote slaptažodį?"/>   
+            <input type="submit" name="login" class="button" value="Prisijungti"/>   
+            <input type="submit" name="problem" class="button" value="Pamiršote slaptažodį?"/>   
         </p>
         <p>
  <?php
