97305925b8bd616dcb2b19dd57c2d740be459702
gertum <gerda.tumelyte@gmail.com>
2023-12-18 01:02:23 +0200

    module.js beauty purposes; import map has to be in html

--- js/lektuvas/import_map.js
+++
@@ -1,6 +0,0 @@
-{
-    "imports": {
-        "three": "https://unpkg.com/three@0.149.0/build/three.module.js",
-        "three/addons/": "https://unpkg.com/three@0.149.0/examples/jsm/"
-    }
-}
--- lektuvas.php
+++ lektuvas.php
@@ -89,66 +89,8 @@ else {
 
 
     <!-- Your Three.js and OBJLoader-related code -->
-    <script type="module">
-        import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.module.js';
-        import {
-            OBJLoader
-        } from 'https://cdn.jsdelivr.net/npm/three@latest/examples/jsm/loaders/OBJLoader.js';
+    <script src="./js/lektuvas/module.js" type="module"></script>
 
-        import {
-            OrbitControls
-        } from 'https://cdn.jsdelivr.net/npm/three@latest/examples/jsm/controls/OrbitControls.js';
-
-
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer();
-        const controls = new OrbitControls(camera, renderer.domElement);
-
-
-
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
-        scene.add(ambientLight);
-
-        // renderer.setSize(window.innerWidth, window.innerHeight);
-        // document.body.appendChild(renderer.domElement);
-        const container = document.getElementById('container');
-        const canvasWidth = container.offsetWidth - (2 * 100); // Considering left and right padding of 20px
-        const canvasHeight = container.offsetHeight - (2 * 100); // Considering top and bottom padding of 20px
-        renderer.setSize(canvasWidth, canvasHeight);
-        container.appendChild(renderer.domElement);
-
-        const loader = new OBJLoader();
-        loader.load(
-            '3dModels/Airplane1/11803_Airplane_v1_l1.obj',
-            function(object) {
-
-
-
-                object.scale.set(0.001, 0.001, 0.001); // Scale down the object if it's too large
-                scene.add(object);
-            },
-            function(xhr) {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-            },
-            function(error) {
-                console.error('An error happened', error);
-            }
-        );
-
-        camera.position.z = 5;
-
-        const light = new THREE.DirectionalLight(0xffffff); // White directional light
-        light.position.set(0, 1, 1); // Position of the light
-        scene.add(light);
-
-        function animate() {
-            requestAnimationFrame(animate);
-            renderer.render(scene, camera);
-        }
-
-        animate();
-    </script>
 </body>
 
 </html>
