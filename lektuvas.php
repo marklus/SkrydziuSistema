faf7462671c4450b67d9aad49a5bfe07b14a0fe7
gertum <gerda.tumelyte@gmail.com>
2023-12-18 00:13:15 +0200

    page for 3d render is functional +/- now to implement database

--- lektuvas.php
+++ lektuvas.php
@@ -1,3 +1,43 @@
+<?php
+session_start();      // index.php
+// jei vartotojas prisijungęs rodomas demonstracinis meniu pagal jo rolę
+// jei neprisijungęs - prisijungimo forma per include("login.php");
+// toje formoje daugiau galimybių...
+
+include("include/functions.php");
+?>
+<!doctype html>
+
+<link rel="stylesheet" type="text/css" href="stylesUzsakymas.css">
+<link rel="icon" href="./include/icon.ico" type="image/x-icon">
+
+
+<?php
+if (!empty($_SESSION['user']))     //Jei vartotojas prisijungęs, valom logino kintamuosius ir rodom meniu
+{                                  // Sesijoje nustatyti kintamieji su reiksmemis is DB
+    // $_SESSION['user'],$_SESSION['ulevel'],$_SESSION['userid'],$_SESSION['umail']
+
+    $_SESSION['prev']="index";
+
+    include("include/meniu.php"); //įterpiamas meniu pagal vartotojo rolę
+    ?>
+    <?php
+}
+else {
+
+    if (!isset($_SESSION['prev'])) inisession("full");             // nustatom sesijos kintamuju pradines reiksmes
+    else {if ($_SESSION['prev'] != "proclogin") inisession("part"); // nustatom pradines reiksmes formoms
+    }
+    // jei ankstesnis puslapis perdavė $_SESSION['message']
+    echo "<div align=\"center\">";echo "<font size=\"4\" color=\"#ff0000\">".$_SESSION['message'] . "<br></font>";
+
+    echo "<table class=\"center\"><tr><td>";
+    include("include/login.php");                    // prisijungimo forma
+    echo "</td></tr></table></div><br>";
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +46,7 @@
     <link rel="icon" href="./include/icon.ico" type="image/x-icon">
 
     <meta charset="UTF-8">
-    <title>Simple Three.js Scene</title>
+    <title>Lėktuvo 3D modelis</title>
     <style>
         body {
             margin: 500;
@@ -16,22 +56,27 @@
             display: block;
         }
 
-        /* Define the style for the container */
         #container {
-            width: 100px;
-            height: 100px;
-            margin: auto; /* Center the container */
-        }
+        width: 50%; /* Set the width as a percentage of the window's width */
+        max-width: 600px; /* Adjusted maximum width for better visibility */
+        height: 600px; /* Set the height */
+        margin: auto; /* Center the container */
+        border: 100px solid lightblue; /* Optional: Add a border for visual reference */
+
+    }
     </style>
 </head>
 
 <body>
     <!-- Your HTML body content -->
-    <header>Lėktuvo 3d modelis :)</header>
-
-    <div id="container"></div>
-
-    
+    <div>
+    </div>
+    <div id="container">
+        <!-- <canvas id="rendererCanvas">lėktuviukas</canvas> -->
+    </div>
+<div>
+    <p>Šitas lėktuvas yra labai geras, labai gerai skrenda, dar nėra nukritęs. Rekomenduojame visiems nekrentančių lėktuvų mėgėjams.</p>
+</div>
     <!-- Place the Import map at the top -->
     <script type="importmap">
         {
@@ -42,7 +87,6 @@
         }
     </script>
 
-    <p>Šitas lėktuvas yra labai geras, labai gerai skrenda, dar nėra nukritęs. Rekomenduojame visiems nekrentančių lėktuvų mėgėjams.</p>
 
     <!-- Your Three.js and OBJLoader-related code -->
     <script type="module">
@@ -61,11 +105,18 @@
         const renderer = new THREE.WebGLRenderer();
         const controls = new OrbitControls(camera, renderer.domElement);
 
+
+
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
         scene.add(ambientLight);
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+        // renderer.setSize(window.innerWidth, window.innerHeight);
+        // document.body.appendChild(renderer.domElement);
+        const container = document.getElementById('container');
+        const canvasWidth = container.offsetWidth - (2 * 100); // Considering left and right padding of 20px
+        const canvasHeight = container.offsetHeight - (2 * 100); // Considering top and bottom padding of 20px
+        renderer.setSize(canvasWidth, canvasHeight);
+        container.appendChild(renderer.domElement);
 
         const loader = new OBJLoader();
         loader.load(
