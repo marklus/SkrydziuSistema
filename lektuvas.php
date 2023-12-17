77b3a49e4ba25018863d46026dc53df3dd74bec3
gertum <gerda.tumelyte@gmail.com>
2023-12-17 23:21:14 +0200

    airplane is visible on browser

--- lektuvas.php
+++ lektuvas.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Your head content here -->
     <link rel="icon" href="./include/icon.ico" type="image/x-icon">
@@ -7,10 +8,16 @@
     <meta charset="UTF-8">
     <title>Simple Three.js Scene</title>
     <style>
-        body { margin: 0; }
-        canvas { display: block; }
+        body {
+            margin: 0;
+        }
+
+        canvas {
+            display: block;
+        }
     </style>
 </head>
+
 <body>
     <!-- Your HTML body content -->
 
@@ -26,47 +33,43 @@
 
     <!-- Your Three.js and OBJLoader-related code -->
     <script type="module">
-        // Import and use Three.js and OBJLoader here
-        
+                import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.module.js';
+        import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@latest/examples/jsm/loaders/OBJLoader.js';
 
-// Import Three.js modules
-        import * as THREE from 'three';
-        import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-
-        // Create a scene
         const scene = new THREE.Scene();
-
-        // Create a camera
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 5;
-
-        // Create a renderer
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
 
-        // Create a cube geometry and material
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+        const loader = new OBJLoader();
+        loader.load(
+            '3dModels/Airplane1/11803_Airplane_v1_l1.obj',
+            function(object) {
+                object.scale.set(0.001, 0.001, 0.001); // Scale down the object if it's too large
+                scene.add(object);
+            },
+            function(xhr) {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+            },
+            function(error) {
+                console.error('An error happened', error);
+            }
+        );
 
-        // Animation/render loop
+        camera.position.z = 5;
+
+        const light = new THREE.DirectionalLight(0xffffff); // White directional light
+        light.position.set(0, 1, 1); // Position of the light
+        scene.add(light);
+
         function animate() {
             requestAnimationFrame(animate);
-
-            // Rotate the cube for visualization
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-
             renderer.render(scene, camera);
         }
 
         animate();
-
-
-
-
     </script>
 </body>
+
 </html>
