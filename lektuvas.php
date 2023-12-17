ce5867e6c9dc9f6878e0e171919ad66a96fb4ac3
gertum <gerda.tumelyte@gmail.com>
2023-12-17 09:32:34 +0200

    idk something i forgot to commit last time the project was open

--- lektuvas.php
+++ lektuvas.php
@@ -1,16 +1,14 @@
-<?php
-?>
-<!doctype html>
-<script src="https://cdn.jsdelivr.net/npm/three@latest/build/three.min.js"></script>
-
-<link rel="stylesheet" type="text/css" href="stylesUzsakymas.css">
-
-
-
-
-<!doctype html>
-<a class="nav-link" href="index.php" >GRĮŽTI NAMO<i class="fa fa-list-alt"></i>
-
-<!-- <a href ="https://www.vle.lt/uploads/_CGSmartImage/26411_1-8895340c42f66ba3013bbc0c59507d85.jpg">aaaa</a> -->
-<img src="https://www.vle.lt/uploads/_CGSmartImage/26411_1-8895340c42f66ba3013bbc0c59507d85.jpg" alt="Italian Trulli">
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Render Airplane View</title>
+    <link rel="stylesheet" type="text/css" href="stylesUzsakymas.css">
+    <script src="https://cdn.jsdelivr.net/npm/three@latest/build/three.min.js"></script>
+    <script src="js/render_airplane.js"></script> <!-- This will be your script for rendering the airplane -->
+</head>
+<body>
+    <a class="nav-link" href="index.php">GRĮŽTI NAMO<i class="fa fa-list-alt"></i></a>
+    <!-- <img src="https://www.vle.lt/uploads/_CGSmartImage/26411_1-8895340c42f66ba3013bbc0c59507d85.jpg" alt="Italian Trulli"> -->
+</body>
+</html>
